ab558097266d09fd69928c6891aa0b6a3ca3166b
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2025-02-04 00:29:21 +0300

    iter2: add agent for metrics x2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/xantinium/metrix/internal/agent"
+)
+
+func main() {
+	agent := agent.NewMetrixAgent(agent.MetrixAgentOptions{
+		ServerAddr:     ":8080",
+		PollInterval:   2,
+		ReportInterval: 10,
+	})
+
+	agent.Run()
+
+	<-waitForStopSignal()
+	agent.Stop()
+}
+
+func waitForStopSignal() <-chan os.Signal {
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	return stopChan
+}
---
+++ internal/agent/agent.go
@@ -0,0 +1,91 @@
+// Пакет agent содержит реализацию агента для сбора метрик.
+package agent
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/xantinium/metrix/internal/infrastructure/runtimemetrics"
+	"github.com/xantinium/metrix/internal/models"
+)
+
+// MetrixAgentOptions параметры агента метрик.
+type MetrixAgentOptions struct {
+	ServerAddr     string
+	PollInterval   int
+	ReportInterval int
+}
+
+// NewMetrixAgent создаёт новый агент метрик.
+func NewMetrixAgent(opts MetrixAgentOptions) *MetrixAgent {
+	agent := &MetrixAgent{
+		serverAddr:    opts.ServerAddr,
+		metricsSource: runtimemetrics.NewRuntimeMetricsSource(opts.PollInterval),
+	}
+
+	agent.worker = newMetrixAgentWorker(opts.ReportInterval, agent.UpdateMetrics)
+
+	return agent
+}
+
+// MetrixAgent структура, описывающая агент метрик.
+type MetrixAgent struct {
+	serverAddr    string
+	worker        *metrixAgentWorker
+	metricsSource *runtimemetrics.RuntimeMetricsSource
+}
+
+// Run запускает агента метрик.
+func (agent *MetrixAgent) Run() {
+	agent.metricsSource.Run()
+	agent.worker.Run()
+}
+
+// Run прекращает работу агента метрик.
+func (agent *MetrixAgent) Stop() {
+	agent.metricsSource.Stop()
+	agent.worker.Stop()
+}
+
+// UpdateMetrics обновляет метрики на сервере.
+func (agent *MetrixAgent) UpdateMetrics() {
+	metrics := agent.metricsSource.GetSnapshot()
+
+	for _, metric := range metrics {
+		_, err := http.Post(agent.getUpdateMetricHandlerUrl(metric), "text/plain", nil)
+		if err != nil {
+			log.Printf("failed to update metric: %v", err)
+		}
+	}
+}
+
+// getHandlerUrl создаёт URL-адрес для запроса на обновление метрик.
+func (agent MetrixAgent) getUpdateMetricHandlerUrl(metric models.MetricInfo) string {
+	b := strings.Builder{}
+
+	b.WriteString(agent.serverAddr)
+	b.WriteString("/")
+	b.WriteString(string(metric.Type()))
+	b.WriteString("/")
+	b.WriteString(metric.Name())
+	b.WriteString("/")
+
+	switch metric.Type() {
+	case models.Gauge:
+		b.WriteString(floatToStr(metric.GaugeValue()))
+	case models.Counter:
+		b.WriteString(intToStr(metric.CounterValue()))
+	}
+
+	return b.String()
+}
+
+func floatToStr(v float64) string {
+	return strconv.FormatFloat(v, 'f', -1, 64)
+}
+
+func intToStr(v int64) string {
+	return strconv.FormatInt(v, 10)
+}
