ce77996d9976c0261bd4d73b0e5a2da56e616b84
Chapliy Vladislav <vladislav.chapliy@altcraft.com>
2025-06-04 13:28:28 +0300

    iter20 refactor

--- internal/tools/env.go
+++ internal/tools/env.go
@@ -1,7 +1,6 @@
 package tools
 
 import (
-	"fmt"
 	"os"
 	"strings"
 )
@@ -57,35 +56,4 @@ func GetBoolFromEnv(name string) BoolEnvVar {
 	}
 
 	return BoolEnvVar{Exists: true, Value: strings.TrimSpace(strings.ToLower(valueStr)) == "true"}
-}
-
-// BuildInfo информация о сборке.
-type BuildInfo struct {
-	BuildVersion string
-	BuildDate    string
-	BuildCommit  string
-}
-
-// PrintBuildInfo выводит информацию о сборке.
-func PrintBuildInfo(info BuildInfo) {
-	const notApplicable = "N/A"
-
-	buildVersion := info.BuildVersion
-	if buildVersion == "" {
-		buildVersion = notApplicable
-	}
-
-	buildDate := info.BuildDate
-	if buildDate == "" {
-		buildDate = notApplicable
-	}
-
-	buildCommit := info.BuildCommit
-	if buildCommit == "" {
-		buildCommit = notApplicable
-	}
-
-	fmt.Printf("Build version: %s\n", buildVersion)
-	fmt.Printf("Build date: %s\n", buildDate)
-	fmt.Printf("Build commit: %s\n", buildCommit)
 }
